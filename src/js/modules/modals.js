8d66d4d3799c6adb7ae5811ee11e12fefc8a2807
Serhii Vorotnoi <svorotnoy@gmail.com>
2024-10-20 12:34:44 +0300

    step3: modal

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,14 +1,24 @@
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector) {
+  function bindModal(
+    triggerSelector,
+    modalSelector,
+    closeSelector,
+    closeClickOverlay = true
+  ) {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector);
+      close = document.querySelector(closeSelector),
+      window = document.querySelectorAll("[data-modal");
 
     trigger.forEach((item) => {
       item.addEventListener("click", (e) => {
         if (e.target) {
           e.preventDefault();
         }
+        window.forEach((item) => {
+          item.style.display = "none";
+        });
+
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
 
@@ -27,36 +37,55 @@ const modals = () => {
     // });
 
     close.addEventListener("click", () => {
-      modal.style.display = "none";
-        document.body.style.overflow = "";
+      window.forEach((item) => {
+        item.style.display = "none";
+      });
 
-    //   document.body.classList.remove("modal-open");
+      modal.style.display = "none";
+      document.body.style.overflow = "";
+
+      //   document.body.classList.remove("modal-open");
     });
 
     modal.addEventListener("click", (e) => {
-      if (e.target === modal) {
+      if (e.target === modal && closeClickOverlay) {
+        window.forEach((item) => {
+          item.style.display = "none";
+        });
+
         modal.style.display = "none";
-          document.body.style.overflow = "";
+        document.body.style.overflow = "";
 
         // document.body.classList.remove("modal-open");
       }
     });
   }
-    function showModalByTime(selector, time) { 
-        setTimeout(() => {
-          document.querySelector(selector).style.display = "block";
-          document.body.style.overflow = "hidden";
-
-        }, time);
-  
-    }
+  function showModalByTime(selector, time) {
+    setTimeout(() => {
+      document.querySelector(selector).style.display = "block";
+      document.body.style.overflow = "hidden";
+    }, time);
+  }
 
   bindModal(
     ".popup_engineer_btn",
     ".popup_engineer",
     ".popup_engineer .popup_close"
   );
-    bindModal(".phone_link", ".popup", ".popup_close");
-    // showModalByTime(".popup_engineer", 60000); // раскоментировать для задания
+  bindModal(".phone_link", ".popup", ".popup_close");
+  bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
+  bindModal(
+    ".popup_calc_button",
+    ".popup_calc_profile",
+    ".popup_calc_profile_close",
+    false
+  );
+  bindModal(
+    ".popup_calc_profile_button",
+    ".popup_calc_end",
+    ".popup_calc_end_close",
+    false
+  );
+  // showModalByTime(".popup_engineer", 60000); // раскоментировать для задания
 };
 export default modals;
